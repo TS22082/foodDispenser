99bc0e1b29ce1ee02d3b4b5ab6d8894a0ff4722b
Thomas Smith <thomassmith@Thomass-MacBook-Pro.local>
2017-07-16 13:37:24 -0700

    refactored python code and added code to project page

--- servoGo.py
+++ servoGo.py
@@ -9,18 +9,18 @@ GPIO.setup(4, GPIO.OUT)
 
 def servoGo ():
 
-    s = GPIO.PWM(2, 50)
-    n = GPIO.PWM(4, 50)
+    svo1 = GPIO.PWM(2, 50)
+    svo2 = GPIO.PWM(4, 50)
 
-    s.start(0)
-    n.start(0)
+    svo1.start(0)
+    svo2.start(0)
 
-    s.ChangeDutyCycle(12.5)#dutycycle also 2.5, 7.5, and 12.5
-    n.ChangeDutyCycle(12.5)
+    svo1.ChangeDutyCycle(12.5)#dutycycle also 2.5, 7.5, and 12.5
+    svo2.ChangeDutyCycle(12.5)
 
     time.sleep(.75) #servo function time
 
-    s.ChangeDutyCycle(0)# servo off,
-    n.ChangeDutyCycle(0)
+    svo1.ChangeDutyCycle(0)# servo off,
+    svo2.ChangeDutyCycle(0)
 
 servoGo()
